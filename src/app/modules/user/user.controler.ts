d70739a1ad5bdd657a5d0ac04633a1f88ba00157
Antor4q <tariquelislam2015@gmail.com>
2026-06-22 14:02:20 +0600

    send response utitliy func created

--- src/app/modules/user/user.controler.ts
+++ src/app/modules/user/user.controler.ts
@@ -6,6 +6,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/rendResponse";
 
 
 
@@ -29,18 +30,27 @@ import { catchAsync } from "../../utils/catchAsync";
 // }
 const createUser = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
     const user = await UserServices.createUser(req.body)
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
         message: "User Created Successfully",
-        user
+        data: user,
     })
 })
 
 const getAllUsers = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
-    const users = await UserServices.getAllUsers();
-    res.status(httpStatus.OK).json({
+    const result = await UserServices.getAllUsers();
+    // res.status(httpStatus.OK).json({
+    //     success: true,
+    //     message: "All Users Retrieved Successfully",
+    //     data: users
+    // })
+    sendResponse(res, {
         success: true,
+        statusCode: httpStatus.OK,
         message: "All Users Retrieved Successfully",
-        data: users
+        data: result.data,
+        meta: result.meta
     })
 })
 
