57c9c5b13a867b340b3c57f7bf5a0221df79d6f4
Antor4q <tariquelislam2015@gmail.com>
2026-07-20 12:27:23 +0600

    sslcommerz payment api done for booking

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -4,6 +4,7 @@ import { PaymentController } from "./payment.controler";
 
 const router = express.Router()
 
+router.post("/init-payment/:bookingId", PaymentController.initPayment);
 router.post("/success", PaymentController.successPayment);
 router.post("/fail", PaymentController.failPayment);
 router.post("/cancel", PaymentController.cancelPayment);
